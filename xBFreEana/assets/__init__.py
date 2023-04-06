8248250ee9ce70c89ef9428e196bf00ba9a12642
Mario S. Valdés Tresanco <mariosergiovaldes145@gmail.com>
2023-04-06 00:48:41 -0400

    regroup images and icons

---
+++ xBFreEana/assets/__init__.py
@@ -0,0 +1,336 @@
+from pathlib import Path
+
+this = Path(__file__).parent
+
+ArmyRose_7 = this.joinpath('images', 'ArmyRose_7.png').absolute().as_posix()
+ArmyRose_5 = this.joinpath('images', 'ArmyRose_5.png').absolute().as_posix()
+Geyser_7 = this.joinpath('images', 'Geyser_7.png').absolute().as_posix()
+Geyser_5 = this.joinpath('images', 'Geyser_5.png').absolute().as_posix()
+TealRose_7 = this.joinpath('images', 'TealRose_7.png').absolute().as_posix()
+TealRose_5 = this.joinpath('images', 'TealRose_5.png').absolute().as_posix()
+Tropic_7 = this.joinpath('images', 'Tropic_7.png').absolute().as_posix()
+Tropic_5 = this.joinpath('images', 'Tropic_5.png').absolute().as_posix()
+Balance_7 = this.joinpath('images', 'Balance_7.png').absolute().as_posix()
+Balance_5 = this.joinpath('images', 'Balance_5.png').absolute().as_posix()
+Curl_7 = this.joinpath('images', 'Curl_7.png').absolute().as_posix()
+Curl_5 = this.joinpath('images', 'Curl_5.png').absolute().as_posix()
+Delta_7 = this.joinpath('images', 'Delta_7.png').absolute().as_posix()
+Delta_5 = this.joinpath('images', 'Delta_5.png').absolute().as_posix()
+BrBG_7 = this.joinpath('images', 'BrBG_7.png').absolute().as_posix()
+BrBG_5 = this.joinpath('images', 'BrBG_5.png').absolute().as_posix()
+PiYG_7 = this.joinpath('images', 'PiYG_7.png').absolute().as_posix()
+PiYG_5 = this.joinpath('images', 'PiYG_5.png').absolute().as_posix()
+PRGn_7 = this.joinpath('images', 'PRGn_7.png').absolute().as_posix()
+PRGn_5 = this.joinpath('images', 'PRGn_5.png').absolute().as_posix()
+PuOr_7 = this.joinpath('images', 'PuOr_7.png').absolute().as_posix()
+PuOr_5 = this.joinpath('images', 'PuOr_5.png').absolute().as_posix()
+RdBu_7 = this.joinpath('images', 'RdBu_7.png').absolute().as_posix()
+RdBu_5 = this.joinpath('images', 'RdBu_5.png').absolute().as_posix()
+RdGy_7 = this.joinpath('images', 'RdGy_7.png').absolute().as_posix()
+RdGy_5 = this.joinpath('images', 'RdGy_5.png').absolute().as_posix()
+RdYlBu = this.joinpath('images', 'RdYlBu.png').absolute().as_posix()
+RdYlGn = this.joinpath('images', 'RdYlGn.png').absolute().as_posix()
+Spectral = this.joinpath('images', 'Spectral.png').absolute().as_posix()
+coolwarm = this.joinpath('images', 'coolwarm.png').absolute().as_posix()
+
+
+husl = this.joinpath('images', 'husl.png').absolute().as_posix()
+hls = this.joinpath('images', 'hls.png').absolute().as_posix()
+tab20 = this.joinpath('images', 'tab20.png').absolute().as_posix()
+tab20b = this.joinpath('images', 'tab20b.png').absolute().as_posix()
+tab20c = this.joinpath('images', 'tab20c.png').absolute().as_posix()
+Paired = this.joinpath('images', 'Paired.png').absolute().as_posix()
+Set3 = this.joinpath('images', 'Set3.png').absolute().as_posix()
+deep = this.joinpath('images', 'deep.png').absolute().as_posix()
+muted = this.joinpath('images', 'muted.png').absolute().as_posix()
+bright = this.joinpath('images', 'bright.png').absolute().as_posix()
+pastel = this.joinpath('images', 'pastel.png').absolute().as_posix()
+dark = this.joinpath('images', 'dark.png').absolute().as_posix()
+colorblind = this.joinpath('images', 'colorblind.png').absolute().as_posix()
+tab10 = this.joinpath('images', 'tab10.png').absolute().as_posix()
+Pastel1 = this.joinpath('images', 'Pastel1.png').absolute().as_posix()
+Set1 = this.joinpath('images', 'Set1.png').absolute().as_posix()
+Pastel2 = this.joinpath('images', 'Pastel2.png').absolute().as_posix()
+Accent = this.joinpath('images', 'Accent.png').absolute().as_posix()
+Dark2 = this.joinpath('images', 'Dark2.png').absolute().as_posix()
+Set2 = this.joinpath('images', 'Set2.png').absolute().as_posix()
+
+result_file_icon = this.joinpath('icons', 'result_files.svg').absolute().as_posix()
+bar_plot_icon = this.joinpath('icons', 'bar_plot.svg').absolute().as_posix()
+line_plot_icon = this.joinpath('icons', 'line_plot.svg').absolute().as_posix()
+heatmap_plot_icon = this.joinpath('icons', 'heatmap_plot.svg').absolute().as_posix()
+pymol_icon = this.joinpath('icons', 'pymol_icon.svg').absolute().as_posix()
+result_table_icon = this.joinpath('icons', 'result_table.svg').absolute().as_posix()
+restore_icon = this.joinpath('icons', 'restore.svg').absolute().as_posix()
+save_default_config = this.joinpath('icons', 'save_default_config.svg').absolute().as_posix()
+default_config = this.joinpath('icons', 'default_config.svg').absolute().as_posix()
+save_user_config = this.joinpath('icons', 'save_user_config.svg').absolute().as_posix()
+user_config = this.joinpath('icons', 'user_config.svg').absolute().as_posix()
+alert = this.joinpath('icons', 'alert.svg').absolute().as_posix()
+config = this.joinpath('icons', 'cog.svg').absolute().as_posix()
+
+toc_img = this.joinpath('images', 'TOC.png').absolute().as_posix()
+logo = this.joinpath('icons', 'logo.svg').absolute().as_posix()
+colors_icons = [ArmyRose_7, ArmyRose_5, Geyser_7, Geyser_5]
+
+
+
+tooltip1 = '''
+            <html lang="en">
+
+<style>
+    table, th {
+        border-bottom: 2px solid black;
+        /*border-top: 2px solid black;*/
+    }
+    img {
+        width: 300px;
+        height: 25px;
+    }
+</style>
+<body>
+<h3>Palettes</h3>
+<p>Tooltip</p>''' + f'''
+<table>
+     <tr>
+        <th colspan="5">Cartoon Colors</th>
+    </tr>
+    <tr>
+        <td>ArmyRose_7</td>
+        <td><img src='{ArmyRose_7}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>ArmyRose_5</td>
+        <td><img src='{ArmyRose_5}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <td>Geyser_7</td>
+        <td><img src='{Geyser_7}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>Geyser_5</td>
+        <td><img src='{Geyser_5}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <td>TealRose_7</td>
+        <td><img src='{TealRose_7}' width="200" height="16" 
+        alt=""></td>
+        <td></td>
+        <td>TealRose_5</td>
+        <td><img src='{TealRose_5}' width="200" height="16" 
+        alt=""></td>
+    </tr>
+    <tr>
+        <td>Tropic_7</td>
+        <td><img src='{Tropic_7}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>Tropic_5</td>
+        <td><img src='{Tropic_5}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <th colspan="5">CMOcean Colors</th>
+    </tr>
+    <tr>
+        <td>Balance_7</td>
+        <td><img src='{Balance_7}' width="200" height="16" 
+        alt=""></td>
+        <td></td>
+        <td>Balance_5</td>
+        <td><img src='{Balance_5}' width="200" height="16" 
+        alt=""></td>
+    </tr>
+    <tr>
+        <td>Curl_7</td>
+        <td><img src='{Curl_7}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>Curl_5</td>
+        <td><img src='{Curl_5}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <td>Delta_7</td>
+        <td><img src='{Delta_7}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>Delta_5</td>
+        <td><img src='{Delta_5}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <th colspan="5">Colorbrew Colors</th>
+    </tr>
+    <tr>
+        <td>BrBG_7</td>
+        <td><img src='{BrBG_7}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>BrBG_5</td>
+        <td><img src='{BrBG_5}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <td>PiYG_7</td>
+        <td><img src='{PiYG_7}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>PiYG_5</td>
+        <td><img src='{PiYG_5}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <td>PRGn_7</td>
+        <td><img src='{PRGn_7}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>PRGn_5</td>
+        <td><img src='{PRGn_5}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <td>PuOr_7</td>
+        <td><img src='{PuOr_7}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>PuOr_5</td>
+        <td><img src='{PuOr_5}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <td>RdBu_7</td>
+        <td><img src='{RdBu_7}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>RdBu_5</td>
+        <td><img src='{RdBu_5}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <td>RdGy_7</td>
+        <td><img src='{RdGy_7}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>RdGy_5</td>
+        <td><img src='{RdGy_5}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <td>RdYlBu *</td>
+        <td><img src='{RdYlBu}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>RdYlGn *</td>
+        <td><img src='{RdYlGn}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <td>Spectral *</td>
+        <td><img src='{Spectral}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>coolwarm *</td>
+        <td><img src='{coolwarm}' width="200" height="16" alt=""></td>
+    </tr>
+</table>
+
+<p>* No compatible with PyMOL visualization</p>
+</body>
+</html>'''
+tooltip2 = '''
+            <html lang="en">
+
+<style>
+    table, th {
+        border-bottom: 2px solid black;
+        /*border-top: 2px solid black;*/
+    }
+    img {
+        width: 300px;
+        height: 25px;
+    }
+</style>
+<body>
+<h3>Palettes</h3>
+<p>Tooltip</p>''' + f'''
+<table>
+     <tr>
+        <th colspan="5">Color cycle - 20 -</th>
+    </tr>
+    <tr>
+        <td>husl</td>
+        <td><img src='{husl}' width="200" 
+        height="16" alt=""></td>
+        <td></td>
+        <td>hls</td>
+        <td><img src='{hls}' width="200" height="16" 
+        alt=""></td>
+    </tr>
+    <tr>
+        <td>tab20</td>
+        <td><img src='{tab20}' width="200" height="16" 
+        alt=""></td>
+        <td></td>
+        <td>tab20b</td>
+        <td><img src='{tab20b}' width="200" height="16" 
+        alt=""></td>
+    </tr>
+    <tr>
+        <td>tab20c</td>
+        <td><img src='{tab20c}' width="200" height="16" 
+        alt=""></td>
+    </tr>
+    <tr>
+        <th colspan="5">Color cycle - 12 -</th>
+    </tr>
+    <tr>
+        <td>Paired</td>
+        <td><img src='{Paired}' width="200" height="16" 
+        alt=""></td>
+        <td></td>
+        <td>Set3</td>
+        <td><img src='{Set3}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <th colspan="5">Color cycle - 10 -</th>
+    </tr>
+    <tr>
+        <td>deep</td>
+        <td><img src='{deep}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>muted</td>
+        <td><img src='{muted}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <td>bright</td>
+        <td><img src='{bright}' width="200" height="16" 
+        alt=""></td>
+        <td></td>
+        <td>pastel</td>
+        <td><img src='{pastel}' width="200" height="16" 
+        alt=""></td>
+    </tr>
+    <tr>
+        <td>dark</td>
+        <td><img src='{dark}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>colorblind</td>
+        <td><img src='{colorblind}' width="200" height="16" 
+        alt=""></td>
+    </tr>
+    <tr>
+        <td>tab10</td>
+        <td><img src='{tab10}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <th colspan="5">Color cycle - 9 -</th>
+    </tr>
+    <tr>
+        <td>Pastel1</td>
+        <td><img src='{Pastel1}' width="200" height="16" 
+        alt=""></td>
+        <td></td>
+        <td>Set1</td>
+        <td><img src='{Set1}' width="200" height="16" alt=""></td>
+    </tr>
+    <tr>
+        <th colspan="5">Color cycle - 8 -</th>
+    </tr>
+    <tr>
+        <td>Pastel2</td>
+        <td><img src='{Pastel2}' width="200" height="16" 
+        alt=""></td>
+        <td></td>
+        <td>Accent</td>
+        <td><img src='{Accent}' width="200" height="16" 
+        alt=""></td>
+    </tr>
+    <tr>
+        <td>Dark2</td>
+        <td><img src='{Dark2}' width="200" height="16" alt=""></td>
+        <td></td>
+        <td>Set2</td>
+        <td><img src='{Set2}' width="200" height="16" alt=""></td>
+    </tr>
+</table>
+<p>Please</p>
+</body>
+</html>'''
